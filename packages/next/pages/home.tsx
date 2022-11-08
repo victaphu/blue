7784ea92436ad6a6d1ba1763a04f9f99af4b1c05
victa phu <victaphu@victas-MacBook-Air.local>
2022-11-09 10:02:09 +1100

    update: added backstory to the fairy and general cleanup

--- packages/next/pages/home.tsx
+++ packages/next/pages/home.tsx
@@ -9,12 +9,12 @@ import Nft from '../components/Nft';
 const Home: NextPage = () => {
     return (
         <div className="flex flex-col min-h-full items-center justify-center px-4 sm:px-6 lg:px-8">
-            <div className="flex-1 navbar bg-base-100 w-full">
+            <div className="flex-1 navbar bg-base-100 w-full justify-center items-center">
                 <div className="navbar-start">
-                    <a className="btn btn-ghost normal-case text-xl">Blue</a>
+                    <div className="btn btn-ghost normal-case text-xl">Blue</div>
                 </div>
             </div>
-            <div className="flex w-full">
+            <div className="flex w-full justify-center items-center">
                 <Nft />
             </div>
 
--- packages/next/pages/index.tsx
+++ packages/next/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
-          <img className="mx-auto h-48 w-auto" src="/images/screens/splash.svg" alt="Your Company" />
+          <Image className="mx-auto h-48 w-auto" height='320' width='320' src="/images/screens/splash.svg" alt="Your Company" />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or
